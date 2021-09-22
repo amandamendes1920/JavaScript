53eb0a1c79e7ce98a3c6c912ba0cb050d4848b83
Amanda Mendes <76018433+amandamendes1920@users.noreply.github.com>
2021-09-21 21:57:04 -0300

    Add files via upload

---
+++ condicoes.js
@@ -0,0 +1,16 @@
+/*CONDIÇÃO SIMPLES*/
+var vel = 60.5
+console.log(`A  velocidade do seu automóvel é de ${vel}Km/h`)
+if (vel > 60){ 
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log(`Use o cinto de segurança!`)
+
+/*CONDIÇÃO COMPOSTA*/
+var pais  = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}
