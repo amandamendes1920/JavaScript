7affd29fbfd0a08d40bfa0c29dcbf2a7f0a40c3c
Amanda Mendes <76018433+amandamendes1920@users.noreply.github.com>
2021-09-27 21:08:44 -0300

    Add files via upload

---
+++ repeticoesum.js
@@ -0,0 +1,17 @@
+/*var c = 1
+while (c < 6) {
+    console.log('Tudo bem?')
+    c++ //c = c + 1
+}
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 6)*/
+
+console.log('Vai começar')
+for(var c = 1;c<=6;c++){
+    console.log(c)
+}
+console.log('Fim')
